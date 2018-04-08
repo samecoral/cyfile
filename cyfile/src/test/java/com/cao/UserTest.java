a5ece29f9562b84c077b2bce22a1becee5438e45
samecoral <caoheshan1010@163.com>
2018-04-08 15:24:01 +0800

    Revert "Revert "Revert "全部添加"""
    This reverts commit 05610c98e1cca8ecfd63b5b2582c25962702c78c.

---
+++ cyfile/src/test/java/com/cao/UserTest.java
@@ -0,0 +1,51 @@
+package com.cao;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.cao.user.dao.IUserDao;
+import com.cao.user.pojo.User;
+/**
+ * 
+ * @author caoheshan
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class UserTest {
+	@Autowired
+	IUserDao userDao;
+	@Test
+	public void testFindAuthorById() {
+
+		User u = userDao.findUserById(4);
+		System.out.println(u.getUserName());
+
+	}
+	
+	@Test
+	public void testInsertUser(){
+		User u = new User();
+		u.setUserName("caoheshan");
+		u.setGroupId(3);
+		u.setLocked(0);
+		u.setRegDate(new Date());
+		u.setUserPwd("16a995df59c545d7b5a1bb6c467335af");
+		u.setUserSalt("b0abe376c5429e0c07c65d6ea16a4143");
+		userDao.addUser(u);
+	}
+
+	public IUserDao getUserDao() {
+		return userDao;
+	}
+
+	public void setUserDao(IUserDao userDao) {
+		this.userDao = userDao;
+	}
+	
+}
